4a70c7677ae1edb4e65cf3168f7dbb321f1ab08e
node62 <theoregi61@gmail.com>
2024-12-28 20:52:17 +0530

    fix: deleted background.js

--- background.js
+++
@@ -1,5 +0,0 @@
-// background.js
-// For this simple extension, you may not need a background script at all. 
-// But here’s a minimal placeholder if you ever want to add background logic.
-
-console.log("ChatGPT Emotion Banner extension background script loaded.");
